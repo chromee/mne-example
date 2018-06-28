f97228f3fbe47a976a65d1d56596066f918a9bb5
chromee <odk.mmmm@gmail.com>
2018-06-28 14:51:11 +0900

    simulinkからupdで値を受け取れるように

--- test/others/udp.py
+++ test/others/udp.py
@@ -7,14 +7,14 @@ import struct
 def main():
   host = '127.0.0.1'
   port = 4000
-  bufsize = 4096
+  bufsize = 8192
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   with closing(sock):
     sock.bind((host, port))
     while True:
       buf = sock.recv(bufsize)
-      print(struct.unpack('!16s', buf))
+      print(struct.unpack('<1d', buf)[0])
   return
 
 if __name__ == '__main__':
