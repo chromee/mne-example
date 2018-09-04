e7378747bc63b1b105b47fd36e9896b12b8f398d
chromee <odk.mmmm@gmail.com>
2018-09-04 15:40:51 +0900

    リアルイタイム分類調整

--- scripts/sys/realtime_wave.py
+++ scripts/sys/realtime_wave.py
@@ -9,22 +9,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from time import sleep
 
-
-raw = get_raw(subject=1, runs=[6, 10, 14], event_id=dict(hands=2, feet=3))
+subject = 1
+raw = get_raw(subject)
 data = raw.get_data()
 
-sfreq = 512
+sfreq = 40  # 本当は160Hzだけどなぜか時間が合うのは40
 interval = 1. / sfreq
+time_range = 10
 
+ch = 1
 fig, ax = plt.subplots(1, 1)
-time_range = 1
 x = np.arange(0, time_range, interval)
-y = data[1][0:time_range*sfreq]
+y = data[ch][0:time_range*sfreq]
 lines, = ax.plot(x, y)
 
-for i in range(len(data[0])):
+for i in range(len(data[ch])):
     x += interval
-    y = data[1][i:i+time_range*sfreq]
+    y = data[ch][i:i+time_range*sfreq]
     lines.set_data(x, y)
     ax.set_xlim((x.min(), x.max()))
     ax.set_ylim((y.min(), y.max()))
